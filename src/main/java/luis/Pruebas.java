a0c9d9f4df818943dcb55b45587c3ae645c0f636
LuisL16-ui <l20170728@culiacan.tecnm.mx>
2024-04-17 20:38:26 -0700

    Uncomment

--- src/main/java/luis/Pruebas.java
+++ src/main/java/luis/Pruebas.java
@@ -4,29 +4,15 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/**
- * This class represents a GUI application for a race game.
- * It extends JFrame and implements Runnable.
- */
 public class Pruebas extends JFrame implements Runnable{
     private JButton btnIniciar;
     private JLabel lblGanador;
     private JProgressBar [] pBar;
     private Thread [] hilos;
 
-    /*
-     * The main method that starts the application.
-     * @param args command line arguments
-     */
-
     public static void main(String[] args) {
         new Pruebas();
     }
-
-    /*
-     * Constructor for the Pruebas class.
-     * It sets up the JFrame and initializes the GUI components.
-     */
     public Pruebas(){
         super("Carrera de tortugas");
         setSize(500,500);
@@ -34,11 +20,6 @@ public class Pruebas extends JFrame implements Runnable{
         setVisible(true);
         init();
     }
-
-    /*
-     * This method initializes the GUI components.
-     *
-     */
 
     private void init() {
         JPanel contentPane = (JPanel) getContentPane();
@@ -55,7 +36,7 @@ public class Pruebas extends JFrame implements Runnable{
         for (int i = 0; i < pBar.length; i++) {
             c.gridy = i;
             c.gridx = 0;
-            JLabel aux = new JLabel(name + (i + 1)); // Se crea un label con el nombre del caballo
+            JLabel aux = new JLabel(name + (i + 1));
             aux.setBorder(BorderFactory.createEmptyBorder(borderGap, borderGap, borderGap, borderGap));
             panelCentral.add(aux, c);
 
@@ -90,38 +71,19 @@ public class Pruebas extends JFrame implements Runnable{
         add(btnIniciar, BorderLayout.SOUTH);
     }
 
-    /**
-     * The run method for the Runnable interface.
-     * Currently, it does nothing.
-     */
     @Override
     public void run() {
     }
-
-    /**
-     * This class represents a runnable horse for the race game.
-     * It implements Runnable.
-     */
 
 
     class Tortuga implements Runnable {
         private JProgressBar barra;
         private int num;
-        /**
-         * Constructor for the Caballo class.
-         * @param barra the progress bar representing the horse
-         * @param numTortuga the number of the tortuga
-         */
 
         public Tortuga(JProgressBar barra, int numTortuga) {
             this.barra = barra;
             this.num = numTortuga;
         }
-
-        /**
-         * The run method for the Runnable interface.
-         * It updates the progress bar and checks for a winner.
-         */
 
         @Override
         public void run() {
